ae1b53d730dabc98156a3fb1b8721290046a3cb2
kyeongjun-ko <rhrudwnszoq@naver.com>
2025-09-11 01:10:19 +0900

    feat: add AWS S3 image upload & list REST API for boilerplate

---
+++ src/main/java/com/miracle/coordifit/config/MyBatisConfig.java
@@ -0,0 +1,10 @@
+package com.miracle.coordifit.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.miralcle.coordifit.image.repository")
+public class MyBatisConfig {
+
+}
