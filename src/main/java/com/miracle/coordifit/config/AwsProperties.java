fb8bafc6502dd69d5c9dcf6a23187dcb4461ff63
kyeongjun-ko <rhrudwnszoq@naver.com>
2025-09-11 01:03:37 +0900

    chore: add CORS configuration and setup AWS S3 connection

---
+++ src/main/java/com/miracle/coordifit/config/AwsProperties.java
@@ -0,0 +1,14 @@
+package com.miracle.coordifit.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Configuration
+@ConfigurationProperties(prefix="aws.s3")
+@Data
+public class AwsProperties {
+	private String bucket;
+	private String region;
+}
