56e8b7c0887600ebae60e534571fe11c9ce929eb
kbs <diehegey3@gmail.com>
2025-09-26 15:03:00 +0900

    add: 인터페이스, 리포지토리, 이미지링크 추가

---
+++ src/main/java/com/miracle/coordifit/clothes/service/IClothesService.java
@@ -0,0 +1,19 @@
+package com.miracle.coordifit.clothes.service;
+
+import com.miracle.coordifit.clothes.model.Clothes;
+import com.miracle.coordifit.common.model.FileInfo;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface IClothesService {
+    String register(Clothes clothes, List<MultipartFile> images, String userId); 
+    void modify(Clothes clothes, List<MultipartFile> addImages, boolean replaceAllImages, String updaterId);
+    void remove(String clothesId);
+
+    Clothes findOne(String clothesId);
+    List<Clothes> findMine(String userId);
+    List<FileInfo> findImages(String clothesId);
+
+    void removeImage(String clothesId, Long fileId); 
+}
