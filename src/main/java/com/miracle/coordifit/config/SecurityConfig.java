3de738e4386915bb026487ab3e232dd9067cbf8b
Geonho Kim <yorjsgh@naver.com>
2025-09-29 17:10:51 +0900

    feat : 레디스, 시큐리티 설정파일 추가, JWT 필터 추가

---
+++ src/main/java/com/miracle/coordifit/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.miracle.coordifit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.miracle.coordifit.auth.filter.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.cors(Customizer.withDefaults())
+
+			.csrf(csrf -> csrf.disable())
+
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+
+			// 인증이 필요한 요청과 허용할 요청 설정
+			.authorizeHttpRequests(auth -> auth
+				// 인증 관련 API는 허용
+				.requestMatchers("/api/auth/**").permitAll()
+				// 나머지 모든 API는 인증 필요
+				.anyRequest().authenticated());
+
+		return http.build();
+	}
+}
