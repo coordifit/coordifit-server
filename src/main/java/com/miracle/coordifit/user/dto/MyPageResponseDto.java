ebaada91f2ec97617e91b23a84971e5d0562f928
GeonHo Kim <yorjsgh@naver.com>
2025-10-11 21:31:50 +0900

    feat : 마이페이지 조회 기능 추가

---
+++ src/main/java/com/miracle/coordifit/user/dto/MyPageResponseDto.java
@@ -0,0 +1,25 @@
+package com.miracle.coordifit.user.dto;
+
+import java.util.List;
+
+import com.miracle.coordifit.post.dto.PostDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MyPageResponseDto {
+	private String userId;
+	private String nickname;
+	private String email;
+	private String profileImageUrl;
+	private int postsCount;
+	private List<PostDto> posts;
+}
