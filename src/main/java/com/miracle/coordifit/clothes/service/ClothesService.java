2ec3e54d508cc86a89764cd27b8c1dcf6fb6cc9b
kbs <diehegey3@gmail.com>
2025-10-13 13:31:22 +0900

    feat:옷장api 마무리

--- src/main/java/com/miracle/coordifit/clothes/service/ClothesService.java
+++ src/main/java/com/miracle/coordifit/clothes/service/ClothesService.java
@@ -38,7 +38,7 @@ public class ClothesService implements IClothesService {
 		}
 		validateCategoryOrThrow(req.getCategoryCode());
 
-		final String id = clothesRepository.selectNextClothesId(); // ✅ 시퀀스 기반
+        final String id = clothesRepository.selectNextClothesId();
 
 		Clothes c = new Clothes();
 		c.setClothesId(id);
