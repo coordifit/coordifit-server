7996f7e91588b7be01e6f1f4c91dd3529e8e3f99
Geonho Kim <yorjsgh@naver.com>
2025-10-14 12:41:33 +0900

    feat : 스냅 전체 조회 기능 추가

--- src/main/java/com/miracle/coordifit/post/controller/PostController.java
+++ src/main/java/com/miracle/coordifit/post/controller/PostController.java
@@ -1,8 +1,11 @@
 package com.miracle.coordifit.post.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.miracle.coordifit.common.dto.ApiResponseDto;
 import com.miracle.coordifit.post.dto.PostCreateRequest;
+import com.miracle.coordifit.post.dto.PostDto;
 import com.miracle.coordifit.post.model.Post;
 import com.miracle.coordifit.post.service.IPostService;
 
@@ -23,6 +27,19 @@ import lombok.extern.slf4j.Slf4j;
 public class PostController {
 
 	private final IPostService postService;
+
+	@GetMapping
+	public ResponseEntity<ApiResponseDto<List<PostDto>>> getAllPosts() {
+		try {
+			List<PostDto> posts = postService.getAllPosts();
+			log.info("전체 게시물 조회 완료: {} 개", posts.size());
+			return ResponseEntity.ok(ApiResponseDto.success("전체 게시물 조회 성공", posts));
+		} catch (Exception e) {
+			log.error("전체 게시물 조회 실패", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.body(ApiResponseDto.error("전체 게시물 조회 실패: " + e.getMessage()));
+		}
+	}
 
 	@PostMapping
 	public ResponseEntity<ApiResponseDto<Post>> createPost(
