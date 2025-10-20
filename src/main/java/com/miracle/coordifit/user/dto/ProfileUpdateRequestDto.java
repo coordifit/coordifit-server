6eae3ededb59b90df6b07b973d8562989a4664ee
Geonho Kim <yorjsgh@naver.com>
2025-10-20 09:35:17 +0900

    feat : 파일 관련로직 벡에서 처리하게 변경

--- src/main/java/com/miracle/coordifit/user/dto/ProfileUpdateRequestDto.java
+++ src/main/java/com/miracle/coordifit/user/dto/ProfileUpdateRequestDto.java
@@ -1,12 +1,14 @@
 package com.miracle.coordifit.user.dto;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.Data;
 
 @Data
 public class ProfileUpdateRequestDto {
 	private String nickname;
-	private Long fileId;
 	private String genderCode;
 	private String birthDate;
 	private String isActive;
+	private MultipartFile file;
 }
