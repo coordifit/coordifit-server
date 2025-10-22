26e0d03b875365dd4edc850554bcfe0a9e8444f7
kyeongjun-ko <rhrudwnszoq@naver.com>
2025-10-22 11:33:46 +0900

    feat:  코드 매퍼 & 리포지토리 추가

---
+++ src/main/java/com/miracle/coordifit/coordi/repository/CoordiRepository.java
@@ -0,0 +1,37 @@
+package com.miracle.coordifit.coordi.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.miracle.coordifit.coordi.model.Coordi;
+import com.miracle.coordifit.coordi.model.CoordiItem;
+
+@Mapper
+public interface CoordiRepository {
+	// 전체 코디 조회
+	List<Coordi> getAllCoordisByUser(@Param("userId") String userId);
+
+	// 단건 코디 조회
+	Optional<Coordi> getCoordiById(String coordiId);
+
+	// 코디 생성
+	int insertCoordi(Coordi coordi);
+
+	// 코디 아이템 히스토리
+	int insertCoordiItem(CoordiItem coordiItem);
+
+	// 코디 업데이트
+	int updateCoordiById(Coordi coordi);
+
+	// 코디 삭제
+	int deleteCoordiById(String coordiId);
+
+	// 코디 아이템 삭제
+	int deleteCoordiItemsByLookId(String coordiId);
+
+	// 코디 아이디 생성
+	int getNextCoordiSequence();
+}
