7b749a96547081abe924b7b5fa1562889e5c8bad
Geonho Kim <yorjsgh@naver.com>
2025-11-05 17:54:40 +0900

    fix : 비밀번호 유효성 검사 #^ 추가

--- src/main/java/com/miracle/coordifit/auth/dto/AuthRequestDto.java
+++ src/main/java/com/miracle/coordifit/auth/dto/AuthRequestDto.java
@@ -18,11 +18,11 @@ public class AuthRequestDto {
 	private String email;
 
 	@Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
-	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$", message = "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.")
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&#^])[A-Za-z\\d@$!%*?&#^]+$", message = "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.")
 	private String password;
 
 	@Size(min = 8, message = "새 비밀번호는 최소 8자 이상이어야 합니다.")
-	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$", message = "새 비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.")
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&#^])[A-Za-z\\d@$!%*?&#^]+$", message = "새 비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.")
 	private String newPassword;
 
 	@Size(min = 2, max = 50, message = "닉네임은 2자 이상 50자 이하여야 합니다.")
